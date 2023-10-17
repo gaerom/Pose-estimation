20325264cb484b62bf4f481e8e2f3b3e98986c6b
saeromkim <ksr3597@naver.com>
2023-10-18 00:43:08 +0900

    픽셀 분포 독립적으로 확인

--- HEColor.py
+++ HEColor.py
@@ -1,16 +1,30 @@
 import cv2
+import matplotlib.pyplot as plt
 
 image = cv2.imread('./images/test2.jpeg')
 
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV) # Convert to HSV
+### 원본 이미지 histogram 계산
+hist = cv2.calcHist([image], [0], None, [256], [0,256])
+plt.title('Original Image') # 원본 이미지 histogram graph 출력
+plt.plot(hist, color='r', linewidth=1), plt.show()
+
+
+
+### Convert to HSV
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 h, s, v = cv2.split(hsv) # 각각의 채널로 컬러 영상을 분리
 equalV = cv2.equalizeHist(v) # value(명도)를 equalization
 
 new_hsv = cv2.merge([h, s, equalV]) # 위 equalized v를 사용
 hsv2bgr = cv2.cvtColor(new_hsv, cv2.COLOR_HSV2BGR) # Convert to BGR
 
+hist_hsv = cv2.calcHist([hsv2bgr], [0], None, [256], [0,256]) # HSV histogram 계산
+plt.title('HSV')
+plt.plot(hist_hsv, color='r', linewidth=1), plt.show()
 
 
+
+### Convert to yCrCb
 yCrCb = cv2.cvtColor(image, cv2.COLOR_BGR2YCrCb)
 y, Cr, Cb = cv2.split(yCrCb)
 equalY = cv2.equalizeHist(y) # y(휘도)를 equalization
@@ -18,9 +32,14 @@ equalY = cv2.equalizeHist(y) # y(휘도)를 equalization
 new_yCrCb = cv2.merge([equalY, Cr, Cb])
 yCrCb2bgr = cv2.cvtColor(new_yCrCb, cv2.COLOR_YCrCb2BGR)
 
+hist_ycrcb = cv2.calcHist([yCrCb2bgr], [0], None, [256], [0,256]) # YCrCb histogram 계산
+plt.title('yCrCb')
+plt.plot(hist_ycrcb, color='r', linewidth=1), plt.show()
+
+
+### 결과 출력
 cv2.imshow('original', image)
 cv2.imshow('HSV', hsv2bgr)
 cv2.imshow('yCrCb', yCrCb2bgr)
-cv2.waitKey()
+cv2.waitKey(0)
 cv2.destroyAllWindows()
-

9b1e3dce71c514baeeb981ba73d5932c57dbeac1
saeromkim <ksr3597@naver.com>
2023-10-25 14:40:04 +0900

    Adjusting the brightness of the image

--- bright.py
+++ bright.py
@@ -1,27 +1,25 @@
 import cv2
 import numpy as np
 
-def adjust_brightness(image_path):
+def adjust_brightness(image_path, brightness_factor):
     image = cv2.imread(image_path)
 
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    adjusted_image = cv2.convertScaleAbs(image, alpha=brightness_factor, beta=0)
 
-    # 사람 영역을 위한 mask 생성
-    mask = cv2.threshold(gray_image, 200, 255, cv2.THRESH_BINARY)[1]
+    # Apply Gaussian blur to remove noise
+    blurred_image = cv2.GaussianBlur(adjusted_image, (5, 5), 0)
 
-    # mask 반전
-    mask = cv2.bitwise_not(mask)
+    cv2.imshow('Original Image', image)
+    cv2.imshow('Adjusted Image', adjusted_image)
+    cv2.imshow('Blurred Image', blurred_image)
 
-    # 사람 영역에 밝기 조절을 적용
-    avg = cv2.mean(image)[0] / 2.0
-    person_brightness = cv2.addWeighted(gray_image, 2.0, np.zeros_like(gray_image), 0, -avg)
+    # cv2.imwrite('./images/adjusted_image_new.png', adjusted_image)
+    cv2.imwrite('./images/blurred_image_new.png', blurred_image)
 
-    # 조정된 사람 영역과 배경을 결합
-    result_image = cv2.add(cv2.bitwise_and(image, image, mask=mask), cv2.bitwise_and(cv2.cvtColor(person_brightness, cv2.COLOR_GRAY2BGR), cv2.cvtColor(person_brightness, cv2.COLOR_GRAY2BGR), mask=mask))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    cv2.imshow('original', image)
-    cv2.imshow('adjusted image', result_image)
+image_path = './images/dark_test.jpg'
+brightness_factor = 3.0  # Brightness factor
 
-image_path = './images/test3.jpg'
-adjust_brightness(image_path)
-cv2.waitKey(0)
+adjust_brightness(image_path, brightness_factor)

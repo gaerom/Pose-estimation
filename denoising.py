fed8bac21e9f1d11ce563bf69fdc296d8f8f43bd
saeromkim <ksr3597@naver.com>
2023-10-25 14:49:32 +0900

    Image denoising and save the result

--- denoising.py
+++ denoising.py
@@ -1,9 +1,15 @@
 import cv2
 
-image = cv2.imread('./images/result1.png')
-dst = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
+def denoising(image):
+    dst = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
 
-cv2.imshow('original image', image)
-cv2.imshow('dst', dst)
+    cv2.imshow('original image', image)
+    cv2.imshow('dst', dst)
 
-cv2.imwrite('./images/result2.png', dst)
+    cv2.imwrite('./images/denoised.png', dst)
+
+image = cv2.imread('./images/blurred_image_new.png')
+denoising(image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()

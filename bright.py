87f6718a565350340da95eaa5babaadbd43207d1
saeromkim <ksr3597@naver.com>
2023-10-25 21:09:11 +0900

    Adjusting the brightness for image and video

--- bright.py
+++ bright.py
@@ -1,25 +1,16 @@
 import cv2
-import numpy as np
-
-def adjust_brightness(image_path, brightness_factor):
+def adjust_brightness(image_path, brightness_factor, output_path):
     image = cv2.imread(image_path)
-
     adjusted_image = cv2.convertScaleAbs(image, alpha=brightness_factor, beta=0)
 
     # Apply Gaussian blur to remove noise
     blurred_image = cv2.GaussianBlur(adjusted_image, (5, 5), 0)
+    cv2.imwrite(output_path, blurred_image)
 
-    cv2.imshow('Original Image', image)
-    cv2.imshow('Adjusted Image', adjusted_image)
-    cv2.imshow('Blurred Image', blurred_image)
+    return output_path
 
-    # cv2.imwrite('./images/adjusted_image_new.png', adjusted_image)
-    cv2.imwrite('./images/blurred_image_new.png', blurred_image)
+def adjust_brightness_video(frame, brightness_factor):
+    adjusted_image = cv2.convertScaleAbs(frame, alpha=brightness_factor, beta=0)
+    blurred_image = cv2.GaussianBlur(adjusted_image, (5, 5), 0)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-image_path = './images/dark_test.jpg'
-brightness_factor = 3.0  # Brightness factor
-
-adjust_brightness(image_path, brightness_factor)
+    return blurred_image
